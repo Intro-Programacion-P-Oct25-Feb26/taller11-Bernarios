9e101624b506cd7ee5bb79acb239e66ba64b8b17
danyberna2002@gmail.com <danyberna2002@gmail.com>
2026-01-09 21:18:33 -0500

    Problema5

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,5 +10,34 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] a = obtenersuma(informacion, informacion2);
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%s\t", a[i][j]);
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static int[][] obtenersuma(int[][] a, int[][] b) {
+        int[][] suma = new int[3][3];
+        for (int l = 0; l < a.length; l++) {
+            for (int m = 0; m < a[l].length; m++) {
+
+                int x = a[l][m];
+                int y = b[l][m];
+                suma[l][m] = x + y;
+            }
+
+        }
+        return suma;
+    }
+
 }
